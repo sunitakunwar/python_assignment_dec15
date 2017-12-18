acb397f5dce1ee2f912403c63403859956b0c6e0
sunita <sunitakwr21@gmail.com>
2017-12-18 09:38:21 +0545

    "mix/min"

--- question2.py
+++ question2.py
@@ -1,4 +1,6 @@
-
+'''
+Q2. Given a list of integers, my_list = [4,2,4,0,2,4,5,7,8,9,23,8,5,4,2,2,34,4,45].
+Find the maximum and minimum numbers in the list. (DO NOT USE BUILT-IN MAX, MIN FUNCTIONS)'''
 my_list = [4,2,4,0,2,4,5,7,8,9,23,8,5,4,2,2,34,4,45]
 print ("Max value element : ", max(my_list))
 print("Min value element: ",min(my_list))
